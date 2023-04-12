ac29f4a0abe4d6613065e90bae455d41bf45ebb7
Raul <rauldronk@gmail.com>
2023-04-12 13:26:42 +0300

    Initial Commit

---
+++ GradientBGEffect/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GradientBGEffect
+//
+//  Created by Raul on 12.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GradientBGEffect/GradientBGEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientBGEffectApp.swift
+//  GradientBGEffect
+//
+//  Created by Raul on 12.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct GradientBGEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
